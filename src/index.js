0868964a613700ef0ed2ac0e419743c237f13fc3
Nilugette <nilugette@yahoo.fr>
2021-02-25 10:34:22 +0100

    Refactor

--- src/index.js
+++ src/index.js
@@ -2,22 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  //Initialize state
-  constructor(props) {
-    super(props)
 
-    this.state = { latitude: null, errorMessage: '' }
+  state = { latitude: null, errorMessage: '' }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position)
-        //To update the state I have to use setState
-        this.setState({ latitude: position.coords.latitude })
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message})
-      }
-    );
+      position => this.setState({ latitude: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message}) 
+    )
   }
 
   render() {
